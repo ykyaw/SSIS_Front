0c1512659f4fc279a0eb383636df4732a50d86f0
Thatoe <thatoe@u.nus.edu>
2020-08-12 11:23:56 +0800

    Search added for CAtalogue and Requisition. added StockCards

---
+++ wwwroot/js/Search.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    function Contains(text1, text2) {
+        if (text1.indexOf(text2) != -1)
+            return true;
+    }
+    $("#Search").keyup(function () {
+        var searchText = $("#Search").val().toLowerCase();
+        $(".Search").each(function () {
+            if (!Contains($(this).text().toLowerCase(), searchText)) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        })
+    });
+});
