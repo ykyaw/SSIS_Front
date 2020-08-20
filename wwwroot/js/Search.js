d70c6dffb8048e1fdb01e5d495e2f00db1b0709f
thatoe <thatoe@u.nus.edu>
2020-08-20 12:47:05 +0800

    Search and Filter now works together

--- wwwroot/js/Search.js
+++ wwwroot/js/Search.js
@@ -1,16 +1,14 @@
 ﻿$(document).ready(function () {
-    $('#search').keyup(function () {
-        let search = new RegExp($(this).val(), 'i');
-        $('.search tr').hide();
-        $('.search tr').filter(function () {
-            return search.test($(this).text());
-        }).show();
-    })
-    $('#select').change(function () {
-        let search = new RegExp($(this).val(), 'i');
-        $('.search tr').hide();
-        $('.search tr').filter(function () {
-            return search.test($(this).text());
-        }).show();
+    $('#search, #select').on('input', function () {
+        $('.search').each(function (index) {
+            let search = $(this).html().toLowerCase();
+            let select = $(this).find('td.select').text();
+            console.log(select)
+            if (search.indexOf($('#search').val().toLowerCase()) != -1 && select.indexOf($('#select').val()) != -1) {
+                $(this).fadeIn();
+            } else {
+                $(this).fadeOut();
+            }
+        })
     })
 })
