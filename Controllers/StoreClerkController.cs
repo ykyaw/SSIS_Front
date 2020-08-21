462c7aa66ff15dfb06c911303dfa29ff096317bd
thatoe <thatoe@u.nus.edu>
2020-08-21 11:07:13 +0800

    Store bug fixes

--- Controllers/StoreClerkController.cs
+++ Controllers/StoreClerkController.cs
@@ -310,7 +310,7 @@ namespace SSIS_FRONT.Controllers
         /**
          * @author WUYUDING
          */
-        public IActionResult GenerateRetrieveForm(string errMsg = "")
+        public IActionResult GenerateRetrievalForm(string errMsg = "")
         {
             ViewData["Role"] = CommonConstant.ROLE_NAME[(string)HttpContext.Session.GetString("Role")];
             ViewData["Name"] = (string)HttpContext.Session.GetString("Name");
@@ -319,7 +319,7 @@ namespace SSIS_FRONT.Controllers
             return View();
         }
 
-        public IActionResult RetrieveForm(long date)
+        public IActionResult RetrievalForm(long date)
         {
             //List<RequisitionDetail> requisitionDetails = new List<RequisitionDetail>();
             //RequisitionDetail rd1 = new RequisitionDetail()
@@ -461,7 +461,7 @@ namespace SSIS_FRONT.Controllers
             }
             else
             {
-                return RedirectToAction("GenerateRetrieveForm", "StoreClerk", new { errMsg = result.msg });
+                return RedirectToAction("GenerateRetrievalForm", "StoreClerk", new { errMsg = result.msg });
             }
         }
 
