6b88e2e70ebd0fe4d50cdde3e8028c13e1518717
WUYUDING <e0533355@u.nus.edu>
2020-08-23 00:41:10 +0800

    finish the forecast page

--- wwwroot/js/Forecast.js
+++ wwwroot/js/Forecast.js
@@ -2,9 +2,23 @@
 
     $("#forecast").on("click", function () {
         let itemCode = $("select").val();
-        Get(`http://192.168.1.228:5000/itemcode/${itemCode}`)
+        Get(`http://localhost:5000/ProductId/${itemCode}`)
             .then(function (response) {
                 console.log(response);
+                let { forecast, img } = response;
+                $("img").attr("src", `data:image/png;base64,${img}`)
+                forecast = JSON.parse(forecast);
+                console.log(forecast);
+                let $forecast = "";
+                let sum = 0;
+                let month = new Date().getMonth() + 1;
+                forecast.forEach(function (item, index) {
+                    if (index % 3 == 2) {
+                        $forecast += `<tr><td>${month + Math.floor(index / 3) + 1}</td><td>${sum / 3}</td></tr>`;
+                        sum = 0;
+                    }
+                });
+                $("table").append($forecast);
             })
             .catch(function (err) {
                 console.log("err:", err);
