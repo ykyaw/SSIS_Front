cac31a4ab5959cfe6f9569526ae2fde8fbb0b124
WUYUDING <e0533355@u.nus.edu>
2020-08-22 11:04:52 +0800

    temp stage

---
+++ wwwroot/js/Barchart.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $("#submit").on("click", function () {
+        let itemCode = $("#itemCode").val();
+        let year = $("#year").val();
+        let yearregx = /\d{4}/;
+        if (year == "" || !yearregx.test(year)) {
+            alert("please input a year in a yyyy format");
+            return;
+        }
+        Get(`http://localhost:5000/${itemCode}/${year}`)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (err) {
+                console.error(err);
+            })
+    })
+})
