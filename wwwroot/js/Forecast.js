95205c76aeca32535cd668499e373686795c4f0c
Johnnyqfc <zhangyuanchang0716@gmail.com>
2020-08-25 22:53:44 +0800

    Update forecast in month

--- wwwroot/js/Forecast.js
+++ wwwroot/js/Forecast.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿/*$(document).ready(function () {
 
     $("#forecast").on("click", function () {
         let itemCode = $("select").val();
@@ -19,6 +19,35 @@
                     }
                 });
                 $("table").append($forecast);
+            })
+            .catch(function (err) {
+                console.log("err:", err);
+            })
+    })
+})*/
+
+
+$(document).ready(function () {
+
+    $("#forecast").on("click", function () {
+        let itemCode = $("select").val();
+        Get(`http://localhost:5000/ProductId/${itemCode}`)
+            .then(function (response) {
+                console.log(response);
+                let { forecast, img } = response;
+                $("img").attr("src", `data:image/png;base64,${img}`)
+                forecast = JSON.parse(forecast);
+                console.log(forecast);
+                let $forecast = "";
+                let sum = 0;
+                let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+                forecast.forEach(function (item, index) {
+                    $forecast += `<tr><td>${months[index]}</td><td>${forecast[index]}</td></tr>`;
+
+                });
+                $("tbody").html("");
+                $("tbody").append($forecast);
+
             })
             .catch(function (err) {
                 console.log("err:", err);
